997da9f14ff77c9f49b70f39bb5faa9b892ade8e
SivaGuhan <sivasguhan@gmail.com>
2024-01-08 11:05:38 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import App from "./components/App.js";
+import ReactDOM from "react-dom/client"
+
+const root=document.getElementById("root")
+ReactDOM.createRoot(root).render(<App />)
